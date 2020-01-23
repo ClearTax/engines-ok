67d8f0a2bb974a444c44c9bf553d86e1b631b17c
maddhruv <dhruvjainpenny@gmail.com>
2020-01-23 08:02:05 +0530

    lint and throw error on engine not found

--- index.js
+++ index.js
@@ -1,30 +1,65 @@
 #!/usr/bin/env node
-const { execSync } = require('child_process')
-const { satisfies } = require('semver')
-const chalk = require('chalk')
-const { success, error, info } = require('log-symbols')
-const { engines } = require(`${process.cwd()}/package.json`)
+const { execSync } = require("child_process");
+const { satisfies } = require("semver");
+const chalk = require("chalk");
+const { success, error, info } = require("log-symbols");
+
+// eslint-disable-next-line import/no-dynamic-require
+const { engines } = require(`${process.cwd()}/package.json`);
 
 if (!engines) {
-  console.log(`${info} Engines field was not found in your package.json`)
-  process.exit(1)
+  console.log(`${info} Engines field was not found in your package.json`);
+  process.exit(1);
 }
 
-let isntOk = false
+let isntOk = false;
+let installedVersion;
 
-for (let [eng, version] of Object.entries(engines)) {
-  const installedVersion = execSync(`${eng} --version`).toString().trim()
+Object.entries(engines).forEach(engine => {
+  const [eng, version] = engine;
+  try {
+    installedVersion = execSync(`${eng} --version`)
+      .toString()
+      .trim();
+  } catch (err) {
+    isntOk = true;
+    console.log(
+      chalk.red(`${error} Please install the required engines: ${eng}`)
+    );
+  }
 
   if (satisfies(installedVersion, version)) {
-    console.log(chalk.green(`${success} The ${chalk.blue(eng)} version(${installedVersion}) matches the required version(${version})`))
+    console.log(
+      chalk.green(
+        `${success} The ${chalk.blue(
+          eng
+        )} version(${installedVersion}) matches the required version(${version})`
+      )
+    );
   } else {
-    isntOk = true
-    console.log(chalk.red(`${error} Oops! The ${chalk.blue(eng)} version(${installedVersion}) doesn't match with the required version(${version})`))
+    isntOk = true;
+    console.log(
+      chalk.red(
+        `${error} Oops! The ${chalk.blue(
+          eng
+        )} version(${installedVersion}) doesn't match with the required version(${version})`
+      )
+    );
   }
-}
+});
 
 if (isntOk) {
-  console.log(chalk.yellow('---------------------------------------------------------------------'))
-  console.log(chalk.white(`Please update the above ☝ ${chalk.red('errored')} engines to the required ones in package.json`))
-  process.exit(1)
+  console.log(
+    chalk.yellow(
+      "---------------------------------------------------------------------"
+    )
+  );
+  console.log(
+    chalk.white(
+      `Please update the above ☝ ${chalk.red(
+        "errored"
+      )} engines to the required ones in package.json`
+    )
+  );
+  process.exit(1);
 }
